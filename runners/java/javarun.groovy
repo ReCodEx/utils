ed79ea531a14251a14bf3f7697011404654554dc
Teyras <Teyras@gmail.com>
2017-09-21 13:01:28 +0200

    javarun: add translation of exceptions to error codes

--- runners/java/javarun.groovy
+++ runners/java/javarun.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+
 import groovy.io.FileType
 import java.lang.reflect.*
 
@@ -57,6 +58,11 @@ if (command.equals("scan")) {
 }
 
 if (command.equals("run")) {
+	String stdin = System.getProperty("codex.stdin");
+	if (stdin != null) {
+		System.setIn(new FileInputStream(stdin));
+	}
+
 	def classes = scanDir(new File("."))
 	if (classes.size() == 0) {
 		System.exit(1)
@@ -67,5 +73,42 @@ if (command.equals("run")) {
 	Object[] mainArgs = Arrays.copyOfRange(args, 1, args.length)
 	main.setAccessible(true)
 
-	main.invoke(null, [ mainArgs ] as Object[])
+	try {
+		main.invoke(null, [ mainArgs ] as Object[])
+	} catch (OutOfMemoryError ex) {
+		System.exit(100)
+	} catch (SecurityException ex) {
+		System.exit(101)
+	} catch (ClassNotFoundException ex) {
+		System.exit(102)
+	} catch (NoSuchMethodException ex) {
+		System.exit(103)
+	} catch (IllegalAccessException ex) {
+		System.exit(104)
+	} catch (IllegalArgumentException ex) {
+		System.exit(105)
+	} catch (InvocationTargetException ex) {
+		Throwable cause = ex.getCause()
+		if (cause instanceof StackOverflowError) {
+			System.exit(106)
+		} else if (cause instanceof ArrayIndexOutOfBoundsException) {
+			System.exit(107)
+		} else if (cause instanceof IndexOutOfBoundsException) {
+			System.exit(108)
+		} else if (cause instanceof NullPointerException) {
+			System.exit(109)
+		} else if (cause instanceof ArithmeticException) {
+			System.exit(110)
+		} else if (cause instanceof OutOfMemoryError) {
+			System.exit(111)
+		} else if (cause instanceof SecurityException) {
+			System.exit(112)
+		} else if (cause instanceof IOException) {
+			System.exit(113)
+		} else {
+			System.exit(2)
+		}
+	} catch (Throwable ex) {
+		System.exit(1)
+	}
 }

330cc717e6e07c4aca638a0df76d62bf9340a6e9
Petr Stefan <ptr.stef@gmail.com>
2017-11-02 16:39:04 +0100

    Update broker-stats.py

--- munin/broker-stats.py
+++ munin/broker-stats.py
@@ -5,15 +5,18 @@ import zmq
 if __name__ == "__main__":
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
+    socket.connect("tcp://127.0.0.1:9658")
 
     socket.send_multipart([b"get-runtime-stats"])
     reply = socket.recv_multipart()
+    socket.disconnect("tcp://127.0.0.1:9658")
 
     data = {}
 
-    for key in reply:
-        data[key] = next(reply)
+    it = iter(reply)
+    for key in it:
+        data[key] = next(it)
 
-    print("broker.evaluated", data["evaluated-jobs"])
-    print("broker.failed", data["failed-jobs"])
-    print("broker.queued", data["queued-jobs"])
+    print("broker.evaluated", data[b"evaluated-jobs"].decode('ascii'))
+    print("broker.failed", data[b"failed-jobs"].decode('ascii'))
+    print("broker.queued", data[b"queued-jobs"].decode('ascii'))

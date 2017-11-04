4e6a4269cff364e9f117cd724c706097470a8238
Teyras <Teyras@gmail.com>
2017-11-04 19:42:36 +0100

    add config to munin plugin

--- munin/broker-stats.py
+++ munin/broker-stats.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
 import zmq
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) >= 2 and sys.argv[1] == "config":
+        print("graph_title Broker job counters")
+        print("graph_vlabel jobs")
+        print("graph_category broker")
+        print("evaluated.label Jobs evaluated")
+        print("failed.label Failed jobs")
+        print("queued.label Jobs in queue")
+        sys.exit(0)
+
+    broker_address = "tcp://127.0.0.1:9658"
+
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
-    socket.connect("tcp://127.0.0.1:9658")
+    socket.connect(broker_address)
 
     socket.send_multipart([b"get-runtime-stats"])
     reply = socket.recv_multipart()
-    socket.disconnect("tcp://127.0.0.1:9658")
+    socket.disconnect(broker_address)
 
     data = {}
 
@@ -17,6 +29,6 @@ if __name__ == "__main__":
     for key in it:
         data[key] = next(it)
 
-    print("broker.evaluated", data[b"evaluated-jobs"].decode('ascii'))
-    print("broker.failed", data[b"failed-jobs"].decode('ascii'))
-    print("broker.queued", data[b"queued-jobs"].decode('ascii'))
+    print("evaluated.value", data[b"evaluated-jobs"].decode('ascii'))
+    print("failed.value", data[b"failed-jobs"].decode('ascii'))
+    print("queued.value", data[b"queued-jobs"].decode('ascii'))

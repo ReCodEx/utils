db79b246f132a324e28aac1e985623d5099600e3
Teyras <Teyras@gmail.com>
2017-11-16 16:56:17 +0100

    set an indefinite linger period for brokerctl

--- brokerctl/brokerctl.py
+++ brokerctl/brokerctl.py
@@ -5,19 +5,25 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("You must specify a command (freeze/unfreeze)")
+        print("You must specify a command (freeze/unfreeze)", file=sys.stderr)
         sys.exit(1)
 
     broker_address = "tcp://127.0.0.1:9658"
 
+    if sys.argv[1] == "freeze":
+        command = b"freeze"
+    elif sys.argv[1] == "unfreeze":
+        command = b"unfreeze"
+    else:
+        print("Unknown command", file=sys.stderr)
+        sys.exit(1)
+
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
+    socket.setsockopt(zmq.LINGER, -1)
     socket.connect(broker_address)
 
-    if sys.argv[1] == "freeze":
-        socket.send_multipart([b"freeze"])
-    elif sys.argv[1] == "unfreeze":
-        socket.send_multipart([b"unfreeze"])
+    socket.send_multipart([command])
 
     socket.disconnect(broker_address)
 

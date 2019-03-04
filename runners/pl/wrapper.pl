58646e2768012a38d370d3837b789015747a7039
Adam Dingle <adam@medovina.org>
2019-03-04 19:09:10 +0100

    Update Prolog wrapper
    Changes:
    1. Sort results before printing.
    2. If the query has no solutions, print no ouput and exit process with exit code 0, i.e. success.
    3. Prefix all predicates with recodex_.

--- runners/pl/wrapper.pl
+++ runners/pl/wrapper.pl
@@ -3,26 +3,29 @@
 % Adam Dingle
 
 % Print a single var/val pair, e.g. "X = [3, 6]".
-showVar(Var, Val, N) :- (N > 1 -> write(', ') ; true), write(Var), write(' = '), write(Val).
+recodex_showVar(Var, Val, N) :-
+    (N > 1 -> write(', ') ; true), write(Var), write(' = '), write(Val).
 
 % Print a set of var/val pairs, e.g. "X = [3, 6], Y = [2, 5]".
-showVars(VarNames, Solution) :- length(VarNames, N), numlist(1, N, Nums),
-                                maplist(showVar, VarNames, Solution, Nums), nl.
+recodex_showVars(VarNames, Solution) :-
+    length(VarNames, N), numlist(1, N, Nums),
+    maplist(recodex_showVar, VarNames, Solution, Nums), nl.
 
 % Handle queries with no variables: just print 'true' or 'false'.
-perform_query(Query, [], []) :-
+recodex_perform_query(Query, [], []) :-
     (call(Query) -> writeln('true') ; writeln('false')).
 
 % Handle queries with variables.
-perform_query(Query, Vars, VN) :-
+recodex_perform_query(Query, Vars, VN) :-
     Vars = [_ | _],  % non-empty list
     maplist(arg(1), VN, VarNames),
-    call(bagof(Vars, Query, Solutions)),  % find all solutions
-    maplist(showVars(VarNames), Solutions).     % print them nicely
+    (call(bagof(Vars, Query, Solutions)) -> true ; Solutions = []),  % find all solutions
+    sort(Solutions, SortedSolutions),
+    maplist(recodex_showVars(VarNames), SortedSolutions).     % print them nicely
 
 % Read a query from standard input, gather its solutions, and print them to standard output.
-main :-
+recodex_main :-
   prompt(_, ''),    % disable input prompt
   read_term(Query, [variables(Vars), variable_names(VN)]),
-  perform_query(Query, Vars, VN),
+  recodex_perform_query(Query, Vars, VN),
   halt.

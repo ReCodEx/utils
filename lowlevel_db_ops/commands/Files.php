906a40c1bcca302dba338a883cd4399fe0109656
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2025-04-19 20:56:31 +0200

    Adding routine to fix solution manifest files in plagiarism checks.

--- lowlevel_db_ops/commands/Files.php
+++ lowlevel_db_ops/commands/Files.php
@@ -36,4 +36,80 @@ class Files extends BaseCommand
 			}
 		}
     }
+
+	private function fixSolutionsBatch($rows, $solutionIdx, &$fixedData)
+	{
+		if (!$rows) {
+			return;
+		}
+
+		$solutionIds = array_map(function ($row) use ($solutionIdx) {
+			return $row[$solutionIdx];
+		}, $rows);
+		$translation = $this->db->fetchPairs('SELECT id, assignment_id FROM assignment_solution WHERE id IN (?)', $solutionIds);
+		foreach ($rows as $row) {
+			$assignmentId = $translation[$row[$solutionIdx]];
+			if (!$assignmentId) {
+				echo "Unable to find assignment for solution ID {$row[$solutionIdx]}.\n";
+				exit;
+			}
+
+			$row[] = $assignmentId;
+			$fixedData[] = $row;
+		}
+	}
+
+	public function fixSolutionsManifest($file, $separator = ',', $solutionCol = 'solution_id', $assignmentCol = 'assignment_id')
+	{
+		// load
+		echo "Loading manifest file '$file'...\n";
+		$fp = fopen($file, 'r');
+		$header = fgetcsv($fp, 65536, $separator);
+		$headerIndex = array_flip($header);
+		if (array_key_exists($assignmentCol, $headerIndex)) {
+			echo "Assignment column '$assignmentCol' already present in the manifest.\n";
+			return;
+		}
+		if (!array_key_exists($solutionCol, $headerIndex)) {
+			echo "Solution column '$solutionCol' not found in the manifest, unable to proceed.\n";
+			return;
+		}
+
+		$data = [];
+		$counter = 1;
+		while (($row = fgetcsv($fp, 65536, $separator)) !== false) {
+			++$counter;
+			if (count($row) !== count($header)) {
+				echo "Row #$counter has different number of columns than the header!\n";
+				return;
+			}
+			$data[] = $row;
+		}
+		fclose($fp);
+
+		// fix
+		echo "Fixing data...\n";
+		$fixedData = [];
+		$solutionIdx = $headerIndex[$solutionCol];
+
+		$batch = [];
+		foreach ($data as $row) {
+			$batch[] = $row;
+			if (count($batch) >= 20) {
+				$this->fixSolutionsBatch($batch, $solutionIdx, $fixedData);
+				$batch = [];
+			}
+		}
+		$this->fixSolutionsBatch($batch, $solutionIdx, $fixedData);
+
+		// save
+		echo "Saving fixed data back to $file ...\n";
+		$fp = fopen($file, 'w');
+		$header[] = $assignmentCol;
+		fputcsv($fp, $header, $separator);
+		foreach ($fixedData as $row) {
+			fputcsv($fp, $row, $separator);
+		}
+		fclose($fp);
+	}
 }
